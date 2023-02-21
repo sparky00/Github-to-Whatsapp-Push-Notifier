3841bcdda977f1bfe83bea2a4263bfe60fde796f
sparky00 <sparkylight0@gmail.com>
2023-02-21 15:11:34 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from twilio.rest import Client
+
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
+to_whatsapp_no = os.environ['to_whatsapp_no']
+from_whatsapp_no = os.environ['from_whatsapp_no']
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              body='Code Pushed in main branch of https://github.com/sparky00/Github-to-Whatsapp-Push-Notifier',
+                              from_='whatsapp:'+from_whatsapp_no,
+                              to='whatsapp:'+to_whatsapp_no
+                          )
+
+print("Message ID:",message.sid)
